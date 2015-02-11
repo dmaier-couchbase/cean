94d92a42afca7e665c93fd9a8ee9ebb3d1018690
David Maier <david.maier@couchbase.com>
2015-02-11 08:46:05 +0100

    Added a this to use underscore

--- src/yeoman-generators/generator-cean/angular-controller/templates/_controller.js
+++ src/yeoman-generators/generator-cean/angular-controller/templates/_controller.js
@@ -1,16 +1,1 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name <%= _.slugify(_.humanize(appname)) %>.controller:<%= name =%>
- * @description
- * # <%= name %>
- */
-var app = angular.module('<%= _.slugify(_.humanize(appname)) %>');
-
-app.controller('<%= name %>', function($scope) {
-    
-    //Your controller code here
-    //...
-    
-});
+<%= name %>
