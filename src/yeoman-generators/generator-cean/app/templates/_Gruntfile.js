542077ce2c90cee3573f76bd83c46e3693c37a01
Simon Leigh <simon.leigh@couchbase.com>
2015-08-20 11:32:55 +0200

    Fixed watch folders so that LiveReload now works.

--- src/yeoman-generators/generator-cean/app/templates/_Gruntfile.js
+++ src/yeoman-generators/generator-cean/app/templates/_Gruntfile.js
@@ -4,45 +4,45 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         express: {
-            'options': {},
-            'web': {'options': {'script': 'app.js'}}
+            options: {},
+            web: {options: {script: 'app.js'}}
         },
         watch: {
             frontend: {
-                'options': {'livereload': true},
-                'files': [
-                    '/public/*.html',
-                    '/public/views/*.html',
-                    '/public/styles/*.css',
-                    '/public/scripts/*.js',
-                    '/public/images/*.{png,jpg,jpeg}'
+                options: {livereload: true},
+                files: [
+                    'public/*.html',
+                    'public/views/*.html',
+                    'public/styles/*.css',
+                    'public/scripts/*.js',
+                    'public/images/*.{png,jpg,jpeg}'
                 ]
             },
             web: {
-                'files': ['routes/*.js'],
-                'tasks': ['express:web'],
-                'options': {
-                    'nospawn': true,
-                    'atBegin': true
+                files: ['routes/*.js'],
+                tasks: ['express:web'],
+                options: {
+                    nospawn: true,
+                    atBegin: true,
                 }
             }
         },
         parallel: {
-            'web': {
-                'options': {'stream': true},
-                'tasks': [
+            web: {
+                options: {stream: true},
+                tasks: [
                     {
-                        'grunt': true,
-                        'args': ['watch:frontend']
+                        grunt: true,
+                        args: ['watch:frontend']
                     },
                     {
-                        'grunt': true,
-                        'args': ['watch:web']
+                        grunt: true,
+                        args: ['watch:web']
                     }
                 ]
             }
         }
     });
-    grunt.registerTask("web", ["parallel:web"]);
-    grunt.registerTask("default", ["web"]);
+    grunt.registerTask('web', ['parallel:web']);
+    grunt.registerTask('default', ['web']);
 }
